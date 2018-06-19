f6ffbc3a6033416e4b26171006ad6efc9b80589f
Christian Hoerl <christian@hoerl.eu>
2018-06-19 23:40:07 +0200

    blubb

--- calc/hmacs.py
+++ calc/hmacs.py
@@ -1,6 +1,17 @@
-import hmac
-import hashlib
-import base64
-dig = hmac.new(b'1234567890', msg=your_bytes_string, digestmod=hashlib.sha256).digest()
+#import hmac
+#import hashlib
+#import base64
+#dig = hmac.new(b'1234567890', msg=your_bytes_string, digestmod=hashlib.sha256).digest()
 #base64.b64encode(dig).decode()      # py3k-mode
 #'Nace+U3Az4OhN7tISqgs1vdLBHBEijWcBeCqL5xN9xg='
+#######################
+
+import hashlib
+secretKey = "1234567890"
+givenString = "foobar"
+m = hashlib.sha256()
+
+# Get string and put into givenString.
+
+m.update(givenString + secretKey)
+m.digest()
